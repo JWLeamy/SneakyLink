5925e866ae7ccfefbfd6b084060ebf387271f800
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-07 21:15:41 -0800

    added models

---
+++ models/pages.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Page extends Model {}
+
+Page.init(
+  {
+    id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      autoincrement: true,
+    },
+    url_1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url_2: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    url_3: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    url_4: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    url_5: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    url_6: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+    modelName: "Pages",
+  }
+);
+
+module.exports = Page;
