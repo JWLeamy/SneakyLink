8f6fc9dbcc038279be0a123d8b164d93d1ccbf8f
mkalik <mbadiankouyate@gmail.com>
2023-02-09 21:26:21 -0800

    added files for links

---
+++ controllers/api/linkRoute.js
@@ -0,0 +1,7 @@
+//route for creating links
+
+const Link = require('../../models/Link');
+const router = require('express').router();
+
+router.get('/', (req, res) => {});
+router.post('/', (req, res) => {});
