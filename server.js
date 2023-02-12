21751c2181f01e6065087b25adfa49580523ee7d
John Leamy <jwleamy@gmail.com>
2023-02-12 14:14:11 -0800

    render

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Hi I am now listening!"));
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
 });
