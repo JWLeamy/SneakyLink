a9ea8a704e84a5cb5874d0a1b81439292e19f8ae
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-15 14:57:37 -0800

    save links to database route

--- models/link.js
+++ models/link.js
@@ -1,39 +1,38 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Link extends Model {}
 //could potentially add a page name
 Link.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: true,
-        },
-        url: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        type: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING,
-            references: {
-                model: 'User',
-                key: 'username',
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: true,
     },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    username: {
+      type: DataTypes.STRING,
+      references: {
+        model: "User",
+        key: "username",
+      },
+    },
+  },
 
-    {
-        sequelize,
-        freezeTableName: true,
-        timestamps: false,
-        modelName: 'Links',
-    }
+  {
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+    modelName: "Links",
+  }
 );
 
 module.exports = Link;
